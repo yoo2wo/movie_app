f67d4b666c05af16f100ab60ed319f6fa3123663
yooja <wogud758787@naver.com>
2021-09-13 15:26:01 +0900

    #6 하는중

--- src/App.js
+++ src/App.js
@@ -1,52 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import Movie from './Movie';
-import "./App.css";
 
-
-class App extends React.Component{
-	state = {
-		isLoading: true,
-		movies: []
-	};
-	getMovies = async () => {
-		const {
-			data : {
-				data : { movies }
-			}
-		} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-		this.setState({ movies, isLoading : false});
-	};
-	componentDidMount(){
-		this.getMovies();
-	}
-	render(){
-		const { isLoading, movies } = this.state;
-		return (
-			<section class="container">
-				{isLoading? (
-				<div class="loader">
-					<span class="loader_text">Loading...</span>
-				</div>
-			) : <div class = "movies">
-				{movies.map(movie =>
-					<Movie
-						key={movie.id}
-			 			id={movie.id}
-						year={movie.year}
-						title={movie.title}
-						summary={movie.summary}
-						poster={movie.medium_cover_image}
-						/>
-					)}
-				</div>
-				}
-
-			</section>
-
-		);
-	}
+function App(){
+	return <span>something</span>
 }
 
 export default App;
