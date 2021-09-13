565b6d2ad25aa3e9087feac8cead7f56362a831f
yooja <wogud758787@naver.com>
2021-09-13 23:53:21 +0900

    6.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App(){
-	return <span>something</span>
+	return <HashRouter>
+		<Navigation />
+		<Route path="/" exact={true} component={Home}/>
+		<Route path="/about" component={About}/>
+	</HashRouter>
 }
 
 export default App;
